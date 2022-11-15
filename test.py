889b214a39b205df3d6ace162603c02dd650778e
aneelaaa <118169767+aneelaaa@users.noreply.github.com>
2022-11-15 13:59:49 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,6 @@
-import unittest
-import xmlrunner
-import io
+import pytest
 
-out = io.BytesIO()
-unittest.main(
-    testRunner=xmlrunner.XMLTestRunner(output=out),
-    failfast=False,
-    buffer=False,
-    catchbreak=False,
-    exit=False)
-
-
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'foo')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        with self.assertRaises(TypeError):
-            s.split(2)
-
-
-if __name__ == '__main__':
-    unittest.main(
-        testRunner=xmlrunner.XMLTestRunner(output='test-reports'),
-        failfast=False,
-        buffer=False,
-        catchbreak=False)
+def test_simple_check():
+    a = 4
+    b = 5
+    assert (a + b) == 9
